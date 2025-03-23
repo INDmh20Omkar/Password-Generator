a61188bc06ff16e66078e25e5b1a827e5dde6722
INDmh20Omkar <107385020+INDmh20Omkar@users.noreply.github.com>
2025-03-23 15:58:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+document.getElementById('generate-button').addEventListener('click', function() {
+    const password = generatePassword(12); // Generate a password of length 12
+    document.getElementById('password').value = password;
+});
